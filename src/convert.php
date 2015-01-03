cdcb6acac7343456abda4ff6c61a6c0a8854a3da
shrimpza <git@shrimpworks.za.net>
2015-01-03 21:19:40 +0200

    remove factors from output

--- src/convert.php
+++ src/convert.php
@@ -6,6 +6,11 @@ $api = new UnitsAPI();
 
 try {
     $result = $api->convert($_GET['value'], $_GET['from'], $_GET['to']);
+
+    // remove factors from output
+    unset($result['from']['factors']);
+    unset($result['to']['factors']);
+
     echo json_encode($result);
 } catch (Exception $e) {
     echo json_encode(array("error" => $e->getMessage()));
